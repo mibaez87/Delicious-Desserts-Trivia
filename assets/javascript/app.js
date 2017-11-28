1b0bc58c7d3a8d31aa8c57667777587b5546d7ca
Michel B <michelb@JB-MBA.local>
2017-11-27 22:00:00 -0500

    post tutoring changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,56 +1,83 @@
-$(document).ready(function () {
+window.onload = (function () {
     //create array of each set of questions and answers
     var triviaArray = [{
             question: "Petit Gateau is French for:",
-            answer: ["small dessert", "small chocolate", "small cake", "tiny cake"]
+            answer: ["small dessert", "small chocolate", "small cake", "tiny cake"],
+            correct: "small cake"
         },
         {
             question: "A perfectly tempered chocolate is known for its:",
-            answer: ["shine", "snap", "smell", "color"]
+            answer: ["shine", "snap", "smell", "color"],
+            correct: "snap"
         },
         {
             question: "This popular dessert was originally made without filling:",
-            answer: ["macaron", "oreo", "whoopie pie", "eclair"]
+            answer: ["macaron", "oreo", "whoopie pie", "eclair"],
+            correct: "macaron"
         },
         {
             question: "Which ingredient is NOT required to bake a cake?",
-            answer: ["butter", "sugar", "eggs", "flour"]
+            answer: ["butter", "sugar", "eggs", "flour"],
+            correct: "flour"
         },
         {
             question: "Which of these is America's favorite pie?",
-            answer: ["pumpkin", "apple", "pizza", "cherry"]
+            answer: ["pumpkin", "apple", "pizza", "cherry"],
+            correct: "apple"
         },
         {
             question: "Where was ice cream first made?",
-            answer: ["USA", "China", "Italy", "France"]
+            answer: ["USA", "China", "Italy", "France"],
+            correct: "China"
         },
         {
             question: "Where was the first chocolate chip cookie made?",
-            answer: ["USA", "China", "Italy", "France"]
+            answer: ["USA", "China", "Italy", "France"],
+            correct: "USA"
         }
     ]
     //create variable for choosing random set from array
     var random = Math.floor(Math.random() * triviaArray.length);
     var solution = triviaArray[random];
+
+    var timer = 20; //tells timer where to start
+    var timerInterval;
+    var index = 0;
+    var correct = 0;
+    var incorrect = 0;
+
+    //create function to start and display timer countdown
+    function startTimer (){
+        timer--;
+        $("#timer").html(timer);
+    }
+
     //create function for displaying a questions/answers set
     function displayQuestion (){
+    $("#timer").html(timer);
     $("#question").html(solution.question);
-    $("#optionOne").html(solution.answer[0]);
-    $("#optionTwo").html(solution.answer[1]);
-    $("#optionThree").html(solution.answer[2]);
-    $("#optionFour").html(solution.answer[3]);
-    }
+    for (var i=0; i < solution.answer.length; i++){
+        var answerButton = $("<button class='answerButton' id='button' data-name='" + solution.answer[i] + "'>" + solution.answer[i] + "</button>");
+        $("#answers").append(answerButton);
+    }}
+
     //create function to display the first question when begin button is clicked
     $("#begin").on("click", function (){
         $("#begin").hide();
         displayQuestion ();
+        timerInterval = setInterval(startTimer, 1000);
     });
-    //create function to determine if user clicked correct answer
-    // $("#optionOne, #optionTwo, #optionThree, #optionFour").on("click", function(){
-    //     if (this.id === true){
-    //         console.log(this.id);
-    //         console.log(true);
-            // $("#optionOne").html("Correct!");
-    //     }
-    // });
+
+    //create on click function to determine if user picked correctly
+    $(document).on("click", "#button", function() {
+        var userPick = $(this).data("name");
+
+        if (userPick == solution.correct){
+            $("#question").html("Correct!");
+            correct++;
+        } else {
+            $("#question").html("Good Guess!<br>The correct answer is: " + solution.correct);
+            incorrect++;
+        }
+      });   
 });
