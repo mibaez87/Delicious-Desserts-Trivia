09ac7510377e7cc84ddd5c7c56c61204266f05ce
Michel B <michelb@JB-MBA.local>
2017-11-28 17:29:22 -0500

    question changes on correct/incorrect but not when timer is 0

--- assets/javascript/app.old.js
+++ assets/javascript/app.old.js
@@ -37,81 +37,71 @@ window.onload = (function () {
         }
     ]
 
-    var timer = 21; //tells timer where to start
     var timerInterval;
     var correct = 0;
     var incorrect = 0;
 
-    //create function to start and display timer countdown
+    //create function to start timer
     function runTimer() {
-        timer--;
-        $("#timer").html(timer);
+        var timer = 16; //tells timer where to start
+        timerInterval = setInterval(decrement, 1000); //tells timer to decrease at interval of 1 second
+        }
+    //create function for timer countdown
+    function decrement (){
+        timer--; //decrease timer by 1
+        $("#timer").html(timer); //displays timer
+        //create conditional for what to do when timer finishes countdown
         if (timer === 0) {
+            stopTimer(); //stops timer
+            incorrect++; //increase wrong answers
             $("#timer").html("You're out of time!<br>");
             $("#question").html("The correct answer is: " + solution.correct);
-            incorrect++;
-            stopTimer();
         }
     }
-
-    //create function to stop the timer
-    function stopTimer() {
-        clearInterval(timerInterval);
-    }
-
-    //create function to display the first question when begin button is clicked
-    function firstQuestion() {
-        $("#begin").on("click", function () {
-            $("#begin").hide();
-            timerInterval = setInterval(runTimer, 1000);
-            displayQuestion();
-        });
-    }
-    firstQuestion();
+        //create function to stop the timer
+        function stopTimer() {
+            clearInterval(timerInterval); //clears timer interval
+            setTimeout(displayQuestion, 3000); //sets timeout of 3 seconds before displaying another question   
+        }
 
     //create function for displaying a questions/answers set
     function displayQuestion() {
-
-        timer = 21;
+        //start the timer when question is displayed
         runTimer();
         //create variable for choosing random set from array
         var random = Math.floor(Math.random() * triviaArray.length);
         var solution = triviaArray[random];
 
-        $("#question").html(solution.question);
+        $("#question").html(solution.question); //displays random question
+        //create for loop to go through answer set belonging to chosen random question to dynamically create buttons with each answer choice
         for (var i = 0; i < solution.answer.length; i++) {
             var answerButton = $("<button class='answerButton' id='button' data-name='" + solution.answer[i] + "'>" + solution.answer[i] + "</button><br>");
-            $("#answers").append(answerButton);
+            $("#answers").append(answerButton); //displays the answer buttons
         }
+        //create function to determine if user chose correct answer
         $(document).on("click", "#button", function () {
+            //create variable to hold value of the button user clicked
             var userPick = $(this).data("name");
 
             if (userPick == solution.correct) {
                 $("#question").html("Correct!");
                 correct++;
                 stopTimer();
-                startQuesTimer();
             } else {
                 $("#question").html("Good Guess!<br>The correct answer is: " + solution.correct);
                 incorrect++;
                 stopTimer();
-                startQuesTimer();
             }
         });
     }
 
-    //create function to count down 3 seconds between questions
-    var questionTimer = 0;
-
-    function startQuesTimer() {
-        timerInterval = setInterval(startQuesTimer, 1000);
-        questionTimer++;
-        if (questionTimer === 5) {
-            stopTimer();
-            timerInterval = setInterval(runTimer, 1000);
-            $("#question, #answers").empty();
+    //create function to display the first question when begin button is clicked
+    function firstQuestion() {
+        $("#begin").on("click", function () {
+            $("#begin").hide();
             displayQuestion();
-            correctOrNot();
-        }
+            timerInterval = setInterval(decrement, 1000);
+        });
     }
+    firstQuestion();
 });
